583dbedaa7fa64009651f6f2a05cc3cf9567ba79
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-01 12:58:02 +0900

    🌱 가로수길 데이터 정리

---
+++ project/src/data/donggoong-n-worge.ts
@@ -0,0 +1,53 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const donggoongNworgePhotos: PhotoInfoType[] = [
+  {
+    src: "/img/donggoong-n-worge/01.jpg",
+    date: "2023.11.10",
+    location: "경주시/원화로",
+    title: "드디어 한복!",
+    content: "한복 대여해서 동궁과월지 방문 ㅎㅎ",
+  },
+  {
+    src: "/img/donggoong-n-worge/02.jpg",
+    date: "2023.11.10",
+    location: "경주시/원화로",
+    title: "동궁샷!",
+    content: "이럴려고 삼각대로 챙겼찌 히히",
+  },
+  {
+    src: "/img/donggoong-n-worge/03.jpg",
+    date: "2023.11.10",
+    location: "경주시/원화로",
+    title: "뽑뽀!",
+    content: "기습뽀뽀닷",
+  },
+  {
+    src: "/img/donggoong-n-worge/04.jpg",
+    date: "2023.11.10",
+    location: "경주시/원화로",
+    title: "볼살자랑 민주",
+    content: "사람 많아서 근접샷도 한 장!",
+  },
+  {
+    src: "/img/donggoong-n-worge/05.jpg",
+    date: "2023.11.10",
+    location: "경주시/원화로",
+    title: "공주 민주",
+    content: "불빛, 배경, 한복, 민주, 완벽!",
+  },
+  {
+    src: "/img/donggoong-n-worge/06.jpg",
+    date: "2023.11.10",
+    location: "경주시/원화로",
+    title: "폴라로이드 도전.!",
+    content: "사진은 희미하지만 기억은 선명해 :)",
+  },
+  {
+    src: "/img/donggoong-n-worge/07.jpg",
+    date: "2023.11.10",
+    location: "경주시/원화로",
+    title: "한복 네컷",
+    content: "네컷사진으로 마무리 ㅎㅎ",
+  },
+];
