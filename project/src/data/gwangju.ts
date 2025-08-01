472a533af71538f7c36e25ee453202849d06f224
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-01 14:21:15 +0900

    🌱 광주 데이터 정리

---
+++ project/src/data/gwangju.ts
@@ -0,0 +1,53 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const gwangjuPhotos: PhotoInfoType[] = [
+  {
+    src: "/img/gwangju/01.jpg",
+    date: "2024.02.24",
+    location: "광주광역시/북구",
+    title: "오드의 마법사 클리어!",
+    content: "아마 첫 방탈출 클리어!",
+  },
+  {
+    src: "/img/gwangju/02.jpg",
+    date: "2024.02.24",
+    location: "광주광역시/북구",
+    title: "첫 탈출 기념사진!",
+    content: "범인 냥냥이 잡아버렸징 ㅎㅋㅎㅋㅎㅋㅎ",
+  },
+  {
+    src: "/img/gwangju/03.jpg",
+    date: "2024.02.24",
+    location: "광주광역시/북구",
+    title: "우리도 MZ",
+    content: "위에서 찍는 네컷사진 우리도 찍었다!",
+  },
+  {
+    src: "/img/gwangju/04.jpg",
+    date: "2024.02.24",
+    location: "광주광역시/북구",
+    title: "말 안듣는 우리",
+    content: "위에서 찍는 네컷사진에서 아래에서 찍었다!",
+  },
+  {
+    src: "/img/gwangju/05.jpg",
+    date: "2024.02.24",
+    location: "광주광역시/북구",
+    title: "기요밍쥬",
+    content: "민주랑 같이 먹는 냉우동! 면이 되게 특이했어!",
+  },
+  {
+    src: "/img/gwangju/06.jpg",
+    date: "2024.02.24",
+    location: "광주광역시/북구",
+    title: "방심은 금물!",
+    content: "사진찍는 틈을 노리고 근접샷 ㅎㅋㅎㅋㅎ",
+  },
+  {
+    src: "/img/gwangju/07.jpg",
+    date: "2024.02.24",
+    location: "광주광역시/북구",
+    title: "첫 연극!",
+    content: "완전 대만족이었던 첫 연극!! 아직도 기억에 남아 :)",
+  },
+];
