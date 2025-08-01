5e4ef3364d685e42f46a2ee6166d66626c2295a9
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-01 12:30:22 +0900

    🌱 달동네 데이터 정리

---
+++ project/src/data/coupplering.ts
@@ -0,0 +1,67 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const couppleringPhotos: PhotoInfoType[] = [
+  {
+    src: "/img/coupplering/01.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "쓰담쓰담~",
+    content: " 동규 손은 약~손! 토토로 배는 똥~배!",
+  },
+  {
+    src: "/img/coupplering/02.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "졸려잉",
+    content: "맨날 졸린 동규씨",
+  },
+  {
+    src: "/img/coupplering/03.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "민주구마",
+    content: "방귀를 많이 뀌진 않아요.(진지)",
+  },
+  {
+    src: "/img/coupplering/04.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "도토리숲!",
+    content: "이름 완전 귀여웠었넹 ㅎㅋㅎㅋㅎ",
+  },
+  {
+    src: "/img/coupplering/05.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "커플링 구매 성공!",
+    content: "헤헤 이젠 얇은 커플링이지롱~",
+  },
+  {
+    src: "/img/coupplering/06.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "마리오 카트!",
+    content: "마리오 카트 발견하고 떼쎄서 타자고 함 ㅎㅎ",
+  },
+  {
+    src: "/img/coupplering/07.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "김민주 선수",
+    content: "압도적인 코너링과 적극적인 몸싸움이 강점.",
+  },
+  {
+    src: "/img/coupplering/08.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "찰칵!",
+    content: "퇴장 전 인증샷 ㅎㅎ",
+  },
+  {
+    src: "/img/coupplering/09.jpg",
+    date: "2024.07.06",
+    location: "서울특별시/송파구",
+    title: "마음만큼은 1등",
+    content: "카트라이더 게임도 못참고 떼써서 쟁취 ㅎㅎ",
+  },
+];
