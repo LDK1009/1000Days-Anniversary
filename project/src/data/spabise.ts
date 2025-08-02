46fbb052f025a93095b62cdb59b9058a4fb961c4
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-02 12:07:25 +0900

    🌱 스파비스 데이터 정리

---
+++ project/src/data/spabise.ts
@@ -0,0 +1,32 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const spabisePhotos: PhotoInfoType[] = [
+  {
+    src: "/img/spabise/01.jpg",
+    date: "2023.08.21",
+    location: "아산시/도고면",
+    title: "튜브 민쥬",
+    content: "튜브타고 왕왕 질주하는 민주 발견!",
+  },
+  {
+    src: "/img/spabise/02.jpg",
+    date: "2023.08.21",
+    location: "아산시/도고면",
+    title: "도주 민쥬",
+    content: "사진사를 피해 도망을 시도하지만 모든 경로가 폐쇄됨 키키",
+  },
+  {
+    src: "/img/spabise/03.jpg",
+    date: "2023.08.21",
+    location: "아산시/도고면",
+    title: "계단샷",
+    content: "사실 물 빠지는곳 ㅎ.ㅎ",
+  },
+  {
+    src: "/img/spabise/04.jpg",
+    date: "2023.08.21",
+    location: "아산시/도고면",
+    title: "살려조라 민쥬",
+    content: "물살에 떠 밀려가는 연얀한 민주ㅎㅋㅎㅋㅎㅋ",
+  },
+];
