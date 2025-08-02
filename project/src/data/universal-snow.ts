7145c02113d3b8110fdd5f9bec9a9d66ee2e6151
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-02 12:12:43 +0900

    🌱 눈대학 데이터 정리

---
+++ project/src/data/universal-snow.ts
@@ -0,0 +1,60 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const universalSnowPhotos: PhotoInfoType[] = [
+  {
+    src: "/img/universal-snow/01.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "수마일~",
+    content: "민주가 그려놓은 스마일 발견 ㅎㅎ",
+  },
+  {
+    src: "/img/universal-snow/02.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "동규❤",
+    content: "김준형에게 파괴되기 전에 바로 찍기!! 빨랐쥬",
+  },
+  {
+    src: "/img/universal-snow/03.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "눈 옹당~!",
+    content: "눈을 배경으로 셀카!",
+  },
+  {
+    src: "/img/universal-snow/04.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "안전제일 눈사람",
+    content: "다같이 만든 눈사람 ㅎㅎ",
+  },
+  {
+    src: "/img/universal-snow/05.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "눈나랑 눈나무",
+    content: "눈 내린 소나무 왕왕 예쁘당",
+  },
+  {
+    src: "/img/universal-snow/06.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "투모로우",
+    content: "진짜 얼어죽는 줄 알았지 ㅜ",
+  },
+  {
+    src: "/img/universal-snow/07.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "눈먹민",
+    content: "눈 먹으면 안되지 아가~ :)",
+  },
+  {
+    src: "/img/universal-snow/08.jpg",
+    date: "2023.11.17",
+    location: "아산시/신창면",
+    title: "땅따먹기",
+    content: "발자국 남기는 애깅",
+  },
+];
