88dea20dedcbde9de7e067c90ee74f751aa9fe54
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-02 11:47:22 +0900

    🌱 경복궁 데이터 정리

---
+++ project/src/data/kyungbokgoong.ts
@@ -0,0 +1,81 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const kyungbokgoongPhotos: PhotoInfoType[] = [
+  {
+    src: "/img/kyungbokgoong/01.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "경복궁!",
+    content: "야경 완전 예뻤엉! 비와서 더 느낌있음!",
+  },
+  {
+    src: "/img/kyungbokgoong/02.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "경복궁 찍는 동규",
+    content: "ㅎㅋㅎㅋㅎㅋ비 맞으면서 와서 다 젖었당",
+  },
+  {
+    src: "/img/kyungbokgoong/03.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "경복셀카!",
+    content: "항상 배경을 다 보이게 하는건 어려웡! 셀카봉을 가지고 다녀야겠어 ㅎㅋㅎ",
+  },
+  {
+    src: "/img/kyungbokgoong/04.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "추억회상",
+    content: "이성계 시절 회상중",
+  },
+  {
+    src: "/img/kyungbokgoong/05.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "관상",
+    content: "우리가 왕이 될 상인가?!",
+  },
+  {
+    src: "/img/kyungbokgoong/06.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "전생에 공주?",
+    content: "전생의 기억이 떠올라버린 민쥬",
+  },
+  {
+    src: "/img/kyungbokgoong/07.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "한입포터",
+    content: "한입만",
+  },
+  {
+    src: "/img/kyungbokgoong/08.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "들켜따!",
+    content: "남의 연애편지 몰래보다 들켰다! 아바다카다브라!",
+  },
+  {
+    src: "/img/kyungbokgoong/09.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "해리포터 셀카!",
+    content: "같이 해리포터 옷 입고 셀카 ㅎ.ㅎ",
+  },
+  {
+    src: "/img/kyungbokgoong/10.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "맵핵",
+    content: "저거 마법의 지도인가?! 이제 나도 해리포터 팬이라궁",
+  },
+  {
+    src: "/img/kyungbokgoong/11.jpg",
+    date: "2024.05.11",
+    location: "서울특별시/종로구",
+    title: "말포이와 핾모니",
+    content: "슬리데린 최고! 슬리데린이 정의다!",
+  },
+];
