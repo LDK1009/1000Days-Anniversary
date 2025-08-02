e7ffeb9ca103798f585d367298bd5efb65c2b29d
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-02 09:48:41 +0900

    🌱 광명 데이터 정리

---
+++ project/src/data/gwangmyeong.ts
@@ -0,0 +1,60 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const gwangmyeongPhotos: PhotoInfoType[] = [
+  {
+    src: "/img/gwangmyeong/01.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "광명동굴 입장!",
+    content: "두근두근 민주와 첫 동굴..!",
+  },
+  {
+    src: "/img/gwangmyeong/02.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "신비로워용",
+    content: "생각보다 신비롭고 예쁘게 꾸며져있어서 신기했당!",
+  },
+  {
+    src: "/img/gwangmyeong/03.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "물어봐 키키",
+    content: "손 넣으면 입을 앙! 다물것 같은 석상이 있어따",
+  },
+  {
+    src: "/img/gwangmyeong/04.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "돈 벌어다준다고?!",
+    content: "만지면 돈 많이 번다길래 호다닥 만져버려찌 키키",
+  },
+  {
+    src: "/img/gwangmyeong/05.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "크왕",
+    content: "드래곤 발견! 완전 드래곤볼에 나오는 용 같았엉",
+  },
+  {
+    src: "/img/gwangmyeong/06.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "민주 꽃길만 걸어",
+    content: "예쁜 미디어 아트가 꾸며져있어서 기분이 완전 좋았당",
+  },
+  {
+    src: "/img/gwangmyeong/07.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "섹시 민주",
+    content: "지금은 단발인데 ㅎㅋㅎㅋㅎ 이 때는 장발!",
+  },
+  {
+    src: "/img/gwangmyeong/08.jpg",
+    date: "2024.07.14",
+    location: "광명시/가학로",
+    title: "강렬한 햇빛.!",
+    content: "나오자마자 뜨거운 태양이 나를 반긴다...",
+  },
+];
