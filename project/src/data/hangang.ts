8c17cec5f99c85109391cf38eca81b037f7b40e1
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-08-02 10:07:16 +0900

    🌱 한강 데이터 정리

---
+++ project/src/data/hangang.ts
@@ -0,0 +1,46 @@
+import { PhotoInfoType } from "@/types/photo";
+
+export const hangangPhotos: PhotoInfoType[] = [
+  {
+    src: "/img/hangang/01.jpg",
+    date: "2024.03.16",
+    location: "서울특별시/영등포구",
+    title: "새침 민주 ㅎㅋㅎ",
+    content: "한강 가는 지하철에서 찰칵!",
+  },
+  {
+    src: "/img/hangang/02.jpg",
+    date: "2024.03.16",
+    location: "서울특별시/영등포구",
+    title: "한강 도착!",
+    content: "생각보다 햇빛이 강렬하당.!",
+  },
+  {
+    src: "/img/hangang/03.jpg",
+    date: "2024.03.16",
+    location: "서울특별시/영등포구",
+    title: "예쁘니까 한 장 더!",
+    content: "예쁘니까 한 장 더 ㅎㅋㅎ",
+  },
+  {
+    src: "/img/hangang/04.jpg",
+    date: "2024.03.16",
+    location: "서울특별시/영등포구",
+    title: "뽀잉~",
+    content: "돗자리 펴기 성공 ㅎㅋㅎ 뽀잉 민주 찍기!",
+  },
+  {
+    src: "/img/hangang/05.jpg",
+    date: "2024.03.16",
+    location: "서울특별시/영등포구",
+    title: "아구 죠아~",
+    content: "밖에 나와서 신난 민주 ㅎㅋㅎ",
+  },
+  {
+    src: "/img/hangang/06.jpg",
+    date: "2024.03.16",
+    location: "서울특별시/영등포구",
+    title: "니 감자 무밨나",
+    content: "북괴군 아임미다",
+  },
+];
